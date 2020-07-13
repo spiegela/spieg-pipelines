78fbaa6d36f55c10b38f9d4db3b5f699084c17e1
Aaron Spiegel <aaron.spiegel@emc.com>
2020-07-13 10:44:59 -0500

    Add haproxy template

--- terraform/capv-template/capv-template.tf
+++ terraform/capv-template/capv-template.tf
@@ -2,6 +2,10 @@ variable "vsphere_server" {
   type = string
 }
 
+variable "template_host" {
+  type = string
+}
+
 variable "vsphere_user" {
   type = string
 }
@@ -15,6 +19,14 @@ variable "capv_template_name" {
 }
 
 variable "capv_image" {
+  type = string
+}
+
+variable "haproxy_template_name" {
+  type = string
+}
+
+variable "haproxy_image" {
   type = string
 }
 
@@ -56,8 +68,8 @@ data "vsphere_compute_cluster" "compute_cluster" {
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-data "vsphere_host" "esx_03" {
-  name          = "esx-03.lan"
+data "vsphere_host" "template_host" {
+  name          = var.template_host
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
@@ -68,7 +80,7 @@ resource "vsphere_resource_pool" "resource_pool" {
 
 resource "vsphere_virtual_machine" "capv-ubuntu-1804" {
   name = var.capv_template_name
-  host_system_id = data.vsphere_host.esx_03.id
+  host_system_id = data.vsphere_host.template_host.id
   resource_pool_id = vsphere_resource_pool.resource_pool.id
   datastore_id = data.vsphere_datastore.datastore_1.id
   datacenter_id = data.vsphere_datacenter.dc.id
@@ -89,3 +101,26 @@ resource "vsphere_virtual_machine" "capv-ubuntu-1804" {
     network_id = data.vsphere_network.storage_access_network.id
   }
 }
+
+resource "vsphere_virtual_machine" "capv-haproxy" {
+  name = var.haproxy_template_name
+  host_system_id = data.vsphere_host.template_host.id
+  resource_pool_id = vsphere_resource_pool.resource_pool.id
+  datastore_id = data.vsphere_datastore.datastore_1.id
+  datacenter_id = data.vsphere_datacenter.dc.id
+
+  wait_for_guest_net_timeout = 0
+  wait_for_guest_ip_timeout = 0
+
+  ovf_deploy {
+    remote_ovf_url = var.haproxy_image
+    disk_provisioning = "thin"
+    ovf_network_map = {
+      "nic0" = data.vsphere_network.vm_network.id
+    }
+  }
+  network_interface {
+    network_id = data.vsphere_network.storage_access_network.id
+  }
+}
+
